8dc269b4c6359cda32fc3fb1ab5461b11877dc91
Zim <zimtsui@gmail.com>
2020-11-30 23:25:37 +0800

    dev

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,10 @@ import Startable from 'startable';
 import Router from '@koa/router';
 import bodyParser from 'koa-bodyparser';
 import { URL } from 'url';
+import { join } from 'path';
+import { once } from 'events';
 // path 的第一个 “/” 有没有视为等效，第二个 “/” 起视为不等效
+const PORT = 12000;
 class Redirector extends Startable {
     constructor() {
         super();
@@ -13,22 +16,24 @@ class Redirector extends Startable {
         this.koa.use(bodyParser({
             enableTypes: ['text'],
         }));
-        this.router.put('/(.*)', async (ctx, next) => {
+        this.router.put('/:name', async (ctx, next) => {
             try {
                 const url = new URL(ctx.request.body);
-                this.map.set(ctx.path, url);
+                this.map.set(ctx.params.name, url);
             }
             catch (e) {
                 ctx.status = 400;
             }
             await next();
         });
-        this.router.get('/(.*)', async (ctx, next) => {
+        this.router.get('/:name/:path*', async (ctx, next) => {
             try {
-                const url = this.map.get(ctx.path);
-                console.assert(url);
+                const url = new URL(this.map.get(ctx.params.name).href);
+                if (ctx.params.path)
+                    url.pathname = join(url.pathname, ctx.params.path);
                 for (const name in ctx.query)
                     url.searchParams.append(name, ctx.query[name]);
+                url.hash = ctx.request.URL.hash;
                 ctx.redirect(url.href);
                 ctx.status = 307;
             }
@@ -40,7 +45,8 @@ class Redirector extends Startable {
         this.koa.use(this.router.routes());
     }
     async _start() {
-        this.server = this.koa.listen(12000);
+        this.server = this.koa.listen(PORT);
+        await once(this.server, 'listening');
     }
     async _stop() {
         this.server.close();
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import Router from '@koa/router';
 import bodyParser from 'koa-bodyparser';
 import { URL } from 'url';
 import { Server } from 'http';
+import { join } from 'path';
+import { once } from 'events';
 
 // path 的第一个 “/” 有没有视为等效，第二个 “/” 起视为不等效
+
+const PORT = 12000;
 
 class Redirector extends Startable {
     private koa = new Koa();
@@ -18,22 +22,23 @@ class Redirector extends Startable {
         this.koa.use(bodyParser({
             enableTypes: ['text'],
         }));
-        this.router.put('/(.*)', async (ctx, next) => {
+        this.router.put('/:name', async (ctx, next) => {
             try {
                 const url = new URL(ctx.request.body);
-                this.map.set(ctx.path, url);
+                this.map.set(ctx.params.name, url);
             } catch (e) {
                 ctx.status = 400;
             }
             await next();
         });
-        this.router.get('/(.*)', async (ctx, next) => {
+        this.router.get('/:name/:path*', async (ctx, next) => {
             try {
-                const url = this.map.get(ctx.path)!;
-                console.assert(url);
-
+                const url = new URL(this.map.get(ctx.params.name)!.href);
+                if (ctx.params.path)
+                    url.pathname = join(url.pathname, ctx.params.path);
                 for (const name in ctx.query)
                     url.searchParams.append(name, ctx.query[name]);
+                url.hash = ctx.request.URL.hash;
                 ctx.redirect(url.href);
                 ctx.status = 307;
             } catch (e) {
@@ -45,7 +50,8 @@ class Redirector extends Startable {
     }
 
     protected async _start() {
-        this.server = this.koa.listen(12000);
+        this.server = this.koa.listen(PORT);
+        await once(this.server, 'listening');
     }
 
     protected async _stop() {
